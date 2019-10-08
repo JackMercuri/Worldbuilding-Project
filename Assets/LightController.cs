bb53a92ea1a46004090693b139d138850209c063
MercuriJack <mercurijack@gmail.com>
2019-10-08 20:20:19 +1100

    HiNGe JoInTS ArE fUN

---
+++ Assets/LightController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public List<Light> lightList = new List<Light>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            foreach(Light thingo in lightList)
+            {
+                thingo.enabled = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            foreach (Light thingo in lightList)
+            {
+                thingo.enabled = false;
+            }
+        }
+    }
+}
